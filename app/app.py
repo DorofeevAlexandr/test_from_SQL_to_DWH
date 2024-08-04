c437a2a1b70f4aed066980d6eabef8da7e494154
DorofeevAlexandr <amd_86@mail.ru>
2024-08-05 00:04:52 +0300

    Добавление  всех вакансий 'Data engineer' в базу

--- app/app.py
+++ app/app.py
@@ -3,9 +3,10 @@ import time
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-from hh_vacancies import get_page_vacancies
-from models import Area, Employer, Vacancies 
+from hh_vacancies import get_page_vacancies, copy_vacancie
 
+
+SEARCH_REQUEST = 'Data engineer'
 
 db_name = 'database'
 db_user = 'username'
@@ -14,67 +15,29 @@ db_host = 'db'
 db_port = '5432'
 
 
-
-def add_postg():
+def load_vacancies_save_postgres():
     # Connecto to the database
     db_string = 'postgresql://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name)
     engine = create_engine(db_string)
     print(engine)
 
     with Session(autoflush=False, bind=engine) as db:
-
-        json_vacancies = get_page_vacancies(text='Python Simmbirsoft', page=0)
-
-        if json_vacancies:
-            print(json_vacancies)
-            for jv in json_vacancies['items']:    
-                print(jv)            
-                vacancie = Vacancies(
-                    name = jv['name'],
-                    hh_id = jv['id'],
-                    published_at = jv['published_at'],
-                    created_at = jv['created_at'],
-                    url = jv['url'],
-                )
-
-                area = Area(
-                    name= jv['area']['name'],
-                    hh_id= jv['area']['id'],
-                    url= jv['area']['url'],
-                )
-                print(*area)
-
-                employer = Employer(
-                    name= jv['employer']['name'],
-                    hh_id= jv['employer']['id'],
-                    url= jv['employer']['url'],
-                )
-        
-        
-
-                vacancie.area = area
-                vacancie.employer = employer
-
-                db.add_all([vacancie])
-
-                #db.add(area)
-                db.commit()
-
+        pages_count = get_page_vacancies(text=SEARCH_REQUEST, page=0)['pages']
+        for page in range(pages_count):
+            json_vacancies = get_page_vacancies(text=SEARCH_REQUEST, page=page)
+            if json_vacancies:
+                print(json_vacancies)
+                for jv in json_vacancies['items']:    
+                    print(jv)            
+                    vacancie = copy_vacancie(jv)
+                    db.add_all([vacancie])
+                    #db.add(area)
+                    db.commit()
 
 
 if __name__ == '__main__':
     time.sleep(5)
     try:
-        add_postg()
+        load_vacancies_save_postgres()
     except Exception as e: 
         print('error', e)
-
-        time.sleep(5)
-    try:
-        add_postg()
-    except Exception as e: 
-        print('error', e)
-            
-
-
-        
--- app/hh_vacancies.py
+++ app/hh_vacancies.py
@@ -1,5 +1,7 @@
 import requests      
-import json          
+import json     
+
+from models import Area, Employer, Vacancies 
 
 
 # Функция для получения вакансий
@@ -7,11 +9,36 @@ def get_page_vacancies(text, page):
     url = 'https://api.hh.ru/vacancies'
     params = {
         'text': text,
-        'per_page': 1,
+        'per_page': 100,
         'page': page
     }
     response = requests.get(url, params=params)
     response.raise_for_status()
     return response.json()
 
-get_page_vacancies(text='Data engineer', page=0)
+
+def copy_vacancie(in_json):
+    vacancie = Vacancies(
+        name = in_json['name'],
+        hh_id = in_json['id'],
+        published_at = in_json['published_at'],
+        created_at = in_json['created_at'],
+        url = in_json['url'],
+    )
+
+    area = Area(
+        name= in_json['area']['name'],
+        hh_id= in_json['area']['id'],
+        url= in_json['area']['url'],
+    )
+    print(*area)
+
+    employer = Employer(
+        name= in_json['employer']['name'],
+        hh_id= in_json['employer']['id'],
+        url= in_json['employer']['url'],
+    )     
+
+    vacancie.area = area
+    vacancie.employer = employer
+    return vacancie
